836d1ddb29b9addc64c9c96abd7d1c237ef5b575
Seif Elkerdany <145226996+Seif-Elkerdany@users.noreply.github.com>
2025-03-28 20:20:00 +0200

    Update demo.sh

--- SAM-6D/demo.sh
+++ SAM-6D/demo.sh
@@ -1,8 +1,8 @@
-export CAD_PATH=./SAM-6D/SAM-6D/Data/Example/obj_000020.ply    # path to a given cad model(mm)
-export RGB_PATH=./SAM-6D/SAM-6D/Data/Example/000020.jpg          # path to a given RGB image
-export DEPTH_PATH=./SAM-6D/SAM-6D/Data/Example/000020_depth.png       # path to a given depth map(mm)
-export CAMERA_PATH=./SAM-6D/SAM-6D/Data/Example/camera1.json    # path to given camera intrinsics
-export OUTPUT_DIR=./SAM-6D/SAM-6D/Data/Example/outputs 
+export CAD_PATH=/kaggle/working/SAM-6D/SAM-6D/Data/Example/obj_000020.ply    # path to a given cad model(mm)
+export RGB_PATH=/kaggle/working/SAM-6D/SAM-6D/Data/Example/000020.jpg          # path to a given RGB image
+export DEPTH_PATH=/kaggle/working/SAM-6D/SAM-6D/Data/Example/000020_depth.png       # path to a given depth map(mm)
+export CAMERA_PATH=/kaggle/working/SAM-6D/SAM-6D/Data/Example/camera1.json    # path to given camera intrinsics
+export OUTPUT_DIR=/kaggle/working/SAM-6D/SAM-6D/Data/Example/outputs 
 
 # Render CAD templates
 cd Render
